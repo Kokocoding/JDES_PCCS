507398717ca3b2425f3d8c06adf70ef9c901f136
stytle704726 <stytle704726@gmail.com>
2023-08-29 11:09:29 +0800

    no message

--- app/src/main/java/com/example/jdes_pccs/MediaFragment.kt
+++ app/src/main/java/com/example/jdes_pccs/MediaFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -17,7 +18,7 @@ class MediaFragment: Fragment(R.layout.fragment_media) {
         return inflater.inflate(R.layout.fragment_media, container, false)
     }
 
-    @SuppressLint("DiscouragedApi")
+    @SuppressLint("DiscouragedApi", "ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -25,14 +26,27 @@ class MediaFragment: Fragment(R.layout.fragment_media) {
 
         for ((index, buttonId) in buttonIds.withIndex()) {
             val button = view.findViewById<Button>(buttonId)
-            button.setOnClickListener {
-                //data組合
-                val byte2 = (index + 1).toByte()
-                val byte3 = (0x09 + byte2).toByte()
-                val byte4 = (0x09 + (byte2*2)).toByte()
-                val data = byteArrayOf(0x7A, byte2, byte3, byte4, 0xFF.toByte())
-                val cmd = byteArrayOf(0xFA.toByte(), 0x00, 0x00, 0x01, 0x00, 0x03,data.count().toByte(), 0xFD.toByte()) + data
-                SocketManager.sendCommand(cmd)
+
+            button.setOnTouchListener{ view, motionEvent ->
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        view.scaleX = 1.1f
+                        view.scaleY = 1.1f
+                    }
+                    MotionEvent.ACTION_UP->{
+                        view.scaleX = 1f
+                        view.scaleY = 1f
+
+                        //data組合
+                        val byte2 = (index + 1).toByte()
+                        val byte3 = (0x09 + byte2).toByte()
+                        val byte4 = (0x09 + (byte2*2)).toByte()
+                        val data = byteArrayOf(0x7A, byte2, byte3, byte4, 0xFF.toByte())
+                        val cmd = byteArrayOf(0xFA.toByte(), 0x00, 0x00, 0x01, 0x00, 0x03,data.count().toByte(), 0xFD.toByte()) + data
+                        SocketManager.sendCommand(cmd)
+                    }
+                }
+                true
             }
         }
     }
