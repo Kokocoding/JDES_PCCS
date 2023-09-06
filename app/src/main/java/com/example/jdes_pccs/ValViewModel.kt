da5e34a3deea2751d9650ab58c7f4b507b82bcd4
stytle704726 <stytle704726@gmail.com>
2023-09-06 09:27:47 +0800

    no message

--- app/src/main/java/com/example/jdes_pccs/ValViewModel.kt
+++ app/src/main/java/com/example/jdes_pccs/ValViewModel.kt
@@ -4,6 +4,5 @@ import androidx.lifecycle.ViewModel
 
 class ValViewModel : ViewModel() {
     val RelaybtnBool = BooleanArray(17) { false }
-    val MachineBtnBool = BooleanArray(1) { false }
     val MuteBool = BooleanArray(5) { false }
 }
