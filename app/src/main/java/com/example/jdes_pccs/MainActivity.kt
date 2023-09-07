de53f432fd5cec157038d744a08dc0b16b4106f3
stytle704726 <stytle704726@gmail.com>
2023-09-07 15:12:20 +0800

    no message

--- app/src/main/java/com/example/jdes_pccs/MainActivity.kt
+++ app/src/main/java/com/example/jdes_pccs/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.jdes_pccs
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val buttonIds = arrayOf(R.id.btnVoice, R.id.btnMachine, R.id.btnMedia, R.id.btnRelay)
+    private val buttonIds = arrayOf(R.id.btnVoice, R.id.btnMachine, R.id.btnMedia, R.id.btnRelay, R.id.btnPower)
+    private val AllCall = arrayOf(R.id.allCallOpen, R.id.allCallClose)
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //連線
         SocketManager.connect("192.168.1.200", 6001)
+
+        var builder = AlertDialog.Builder(this)
 
         //title 按鈕事件
         for (buttonId in buttonIds) {
@@ -35,7 +48,35 @@ class MainActivity : AppCompatActivity() {
                     R.id.btnMachine -> replaceFragment(MachineFragment())
                     R.id.btnRelay -> replaceFragment(RelayFragment())
                     R.id.btnMedia -> replaceFragment(MediaFragment())
+                    R.id.btnPower -> replaceFragment(PowerFragment())
                 }
+            }
+        }
+
+        //allCall按鈕區
+        for(buttonId in AllCall){
+            val button = findViewById<Button>(buttonId)
+
+            button.setOnTouchListener{ AllCallView, motionEvent ->
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        AllCallView.scaleX = 1.1f
+                        AllCallView.scaleY = 1.1f
+                    }
+                    MotionEvent.ACTION_UP->{
+                        AllCallView.scaleX = 1f
+                        AllCallView.scaleY = 1f
+
+                        if(buttonId == R.id.allCallOpen) {
+                            startLoading("open", builder)
+                        }
+
+                        if(buttonId == R.id.allCallClose){
+                            startLoading("close", builder)
+                        }
+                    }
+                }
+                true
             }
         }
     }
@@ -45,5 +86,56 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
         fragmentTransaction.commit()
+    }
+
+    private val progressBarMax = 100
+    private val progressIncrement = 2
+    private val handler = Handler(Looper.getMainLooper())
+    private var currentProgress = 0
+
+    private fun startLoading(mode: String, builder: AlertDialog.Builder) {
+        // 使用 Handler 每秒更新一次进度
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val progressBarLiner = findViewById<LinearLayout>(R.id.progressLiner)
+                val fragmentLiner = findViewById<LinearLayout>(R.id.fragmentLiner)
+                val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+                if (currentProgress < progressBarMax) {
+                    fragmentLiner.visibility = View.GONE
+                    progressBarLiner.visibility = View.VISIBLE
+
+                    currentProgress += progressIncrement
+                    progressBar.progress = currentProgress
+                    handler.postDelayed(this, 200) // 延遲0.2秒
+                } else {
+                    // 载入完成后
+                    fragmentLiner.visibility = View.VISIBLE
+                    progressBarLiner.visibility = View.GONE
+
+                    handler.removeCallbacksAndMessages(null)
+                    currentProgress = 0
+
+                    // 創建dialog
+                    if(mode == "open"){
+                        builder.setTitle("開") // 標題
+                        builder.setMessage("已全開完成") // 内容
+                    }
+                    if(mode == "close"){
+                        builder.setTitle("關") // 標題
+                        builder.setMessage("已全關完成") // 内容
+                    }
+
+                    // dialog 按鈕
+                    builder.setPositiveButton("確定") { dialog, _ ->
+                        dialog.cancel()
+                    }
+
+                    // 顯示dialog
+                    val dialog = builder.create()
+                    dialog.show()
+                }
+            }
+        }, 200) // 延遲0.2秒
     }
 }
---
+++ app/src/main/java/com/example/jdes_pccs/PowerFragment.kt
@@ -0,0 +1,83 @@
+package com.example.jdes_pccs
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+
+class PowerFragment : Fragment() {
+
+    private lateinit var buttonIds: Array<Int>
+    private lateinit var vm: ValViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_power, container, false)
+    }
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    @SuppressLint("DiscouragedApi")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //ViewModel 記憶
+        vm = ViewModelProvider(requireActivity())[ValViewModel::class.java]
+        buttonIds = Array(17) { index -> resources.getIdentifier("button${index + 1}", "id", requireContext().packageName) }
+
+        for ((index, buttonId) in buttonIds.withIndex()) {
+            if(index == 16) continue
+            val textView = view.findViewById<Button>(buttonId)
+            textView.text = getString(R.string.PowerSup, index + 1)
+        }
+
+        //記憶那些電源是開的
+        for ((index, btmB) in vm.RelaybtnBool.withIndex()) {
+            if (btmB){
+                val button = view.findViewById<Button>(buttonIds[index])
+                button.setBackgroundResource(R.drawable.button_context_select_style)
+            }
+        }
+
+        //button Listener
+        for ((index, buttonId) in buttonIds.withIndex()) {
+            val button = view.findViewById<Button>(buttonId)
+            button.setOnClickListener {
+                vm.RelaybtnBool[index] = !vm.RelaybtnBool[index]
+
+                //按鈕UI變更 index=16 全開
+                if(index == 16){
+                    for((i, b) in buttonIds.withIndex()){
+                        if(i != 16) vm.RelaybtnBool[i] = vm.RelaybtnBool[index]
+                        view.findViewById<Button>(b).setBackgroundResource(if(vm.RelaybtnBool[index]) R.drawable.button_context_select_style else R.drawable.button_context_style)
+                    }
+                }else{
+                    //16區都按了=>全開按鈕亮起
+                    val allButtonsTrue = vm.RelaybtnBool.copyOfRange(0, 16).all { it }
+                    if(allButtonsTrue){
+                        vm.RelaybtnBool[16] = true
+                        view.findViewById<Button>(buttonIds[16]).setBackgroundResource(R.drawable.button_context_select_style)
+                    }
+                    //有一顆關閉而且全開打開中=>全開按鈕關閉
+                    if(!vm.RelaybtnBool[index] && vm.RelaybtnBool[16]){
+                        vm.RelaybtnBool[16] = false
+                        view.findViewById<Button>(buttonIds[16]).setBackgroundResource(R.drawable.button_context_style)
+                    }
+
+                    button.setBackgroundResource(if(vm.RelaybtnBool[index]) R.drawable.button_context_select_style else R.drawable.button_context_style)
+                }
+
+                view.findViewById<Button>(buttonIds[16]).text = if(vm.RelaybtnBool[16]) getString(R.string.AllCallOff) else getString(R.string.AllCall)
+
+                //data組合
+                val byte3 = if(index == 16) 0x20.toUByte() else index.toUByte()
+                val byte4 = if(vm.RelaybtnBool[index]) 0x01.toUByte() else 0x02.toUByte()
+                val data = ubyteArrayOf(0xAAu, 0x00u, byte3, byte4, 0x55u)
+                val cmd = ubyteArrayOf(0xFAu, 0x00u, 0x00u, 0x04u, 0x00u, 0x03u, data.count().toUByte(), 0xFDu) + data
+                SocketManager.sendCommand(cmd)
+            }
+        }
+    }
+}
